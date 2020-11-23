b94aae308aae10cdac7b2c9c361a061edbf7932e
jareinnejae <jareinne.lagarto@goabroad.com>
2020-11-23 12:50:51 +0800

    Clean up code

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
           Studykik Data Processor
         </p>
         <UpdatedCPQ_PAC_CLSpent_Merging/>
-        <iframe src="https://www.upwork.com/ab/feed/jobs/rss?q=analytics&sort=recency&paging=0%3B10&api_params=1&securityToken=b4f42b611022e9d44f4e2d2a6ab64b00c56e61bd2b6ee837bfe7a04e13f54ad4d53bbed38f156705a29747f1e017e1881714f6b87c8a6c2d2e26e05dde1d5c72&userUid=424301271808458752&orgUid=424301271812653057"></iframe>
       </header>
     </div>
   );
